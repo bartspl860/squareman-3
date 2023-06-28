0455be188b5b01f01979702eb9ee9708dbe57a61
Michal Motyka <michmot546@student.polsl.pl>
2023-06-28 12:43:50 +0200

    Modyfikacja klienta

--- src/Server/Client.java
+++ src/Server/Client.java
@@ -1,6 +1,8 @@
 package Server;
 
 import Game.GameObject;
+import Game.Player;
+import Game.Point;
 
 import java.io.*;
 import java.net.*;
@@ -10,6 +12,10 @@ public class Client extends Thread{
 
 	private Socket _socket;
 	private Server _server;
+	private Player thisPlayer;
+	private Player anotherPlayer;
+	private ReadFromServer readFromServer;
+	private WriteToServer writeToServer;
 	boolean _connected=true;				//dodałem boola do zarzadznia poloczeniem
 	public Client(Socket soc, Server serv){
 		this._socket = soc;
@@ -21,6 +27,7 @@ public class Client extends Thread{
 		try{
 			while(_connected){
 				DataInputStream dis = new DataInputStream(_socket.getInputStream());
+				DataOutputStream out = new DataOutputStream(_socket.getOutputStream());
 				if(dis.available() > 0){
 					int size = dis.readInt();
 					byte[] data = new byte[size];
@@ -33,6 +40,8 @@ public class Client extends Thread{
 					}
 					
 				}
+				readFromServer = new ReadFromServer(dis);
+				writeToServer = new WriteToServer(out);
 			}
 		} catch (IOException | ClassNotFoundException e){
 			e.printStackTrace();
@@ -64,4 +73,59 @@ public class Client extends Thread{
 	// 		e.printStackTrace();
 	// 	}
 	// }
+
+
+	private class ReadFromServer implements Runnable{
+		private DataInputStream dataInputStream;
+
+		public ReadFromServer(DataInputStream dataInputStream) {
+			this.dataInputStream = dataInputStream;
+			System.out.println("ReadFromServer Runnable created");
+		}
+
+		@Override
+		public void run() {
+			try{
+				while (true){
+					if(anotherPlayer != null){
+						Point point = new Point(dataInputStream.readInt(),dataInputStream.readInt());
+						anotherPlayer.SetPosition(point);
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("IOException from ReadFromServer run()");
+			}
+		}
+	}
+
+	private class WriteToServer implements Runnable{
+		private DataOutputStream dataOutputStream;
+
+		public WriteToServer(DataOutputStream dataOutputStream) {
+			this.dataOutputStream = dataOutputStream;
+			System.out.println("WriteToServer Runnable created");
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true){
+					if(thisPlayer != null){
+						dataOutputStream.writeInt(thisPlayer.GetPosition().x);
+						dataOutputStream.writeInt(thisPlayer.GetPosition().y);
+						dataOutputStream.flush();
+					}
+					try{
+						Thread.sleep(25);
+					} catch (InterruptedException e){
+						System.out.println("InterruptedException from WriteToServer run()");
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("IOException from WriteToServer()");
+			}
+		}
+	}
+
 }
+
